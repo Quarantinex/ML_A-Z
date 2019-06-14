02f8655ae2d67ebf35ab8bb14a833bbd3dd8db55
Quarantinex <abhisheknaiky@gmail.com>
2019-06-14 16:28:50 +0530

    Part 2 - Section 5 - Multiple Linear Reg

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Reg.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Reg.py
@@ -4,4 +4,71 @@ Created on Thu Jun 13 18:22:13 2019
 
 @author: Abhishek_Nayak1
 """
+#Multiple Linear Regression
 
+#Importing the libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import impute
+
+#Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+#print(dataset)
+
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 4].values
+#print(X)
+#print(Y)
+
+
+#Encoding the categoricl data
+#Encoding the Independant Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+#print(X)
+
+
+#Avoiding the dummy variable trap
+X = X[:, 1:]
+
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+#Fitting Multiple Linear Regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+
+#Predicting the Test set results
+Y_pred = regressor.predict(X_test)
+
+#Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
