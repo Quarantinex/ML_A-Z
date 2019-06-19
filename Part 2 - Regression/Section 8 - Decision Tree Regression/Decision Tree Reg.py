8e847ecf84300ac9b4f745204e57a0db7915ed8a
Quarantinex <abhisheknaiky@gmail.com>
2019-06-19 11:08:09 +0530

    Part 2 - Section 8 - Decision Tree Reg

--- Part 2 - Regression/Section 8 - Decision Tree Regression/Decision Tree Reg.py
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/Decision Tree Reg.py
@@ -5,3 +5,28 @@ Created on Tue Jun 18 11:46:13 2019
 @author: Abhishek_Nayak1
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+print(dataset)
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2].values
+print(X)
+print(Y)
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, Y)
+Y_pred = regressor.predict([[6.5]])
+print(Y_pred)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+
+
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
