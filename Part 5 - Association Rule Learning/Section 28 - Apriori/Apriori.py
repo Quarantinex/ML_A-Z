ee55b5d2cc126d1e4751de10fdcfd3f5a1d08b50
Quarantinex <abhisheknaiky@gmail.com>
2019-06-24 11:08:48 +0530

    Part 5 - Section 28 - Apriori

--- Part 5 - Association Rule Learning/Section 28 - Apriori/Apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/Apriori.py
@@ -5,3 +5,14 @@ Created on Fri Jun 21 15:20:38 2019
 @author: Abhishek_Nayak1
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+print(dataset)
+
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+print(transactions)
