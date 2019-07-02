dd4fb0c71bbfba5035ea7cc60a6b99ef50c1364f
Quarantinex <abhisheknaiky@gmail.com>
2019-07-02 12:27:21 +0530

    Part 7 - Section 36 - Natural Language Processing

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural Language Processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural Language Processing.py
@@ -5,3 +5,43 @@ Created on Fri Jun 28 12:16:49 2019
 @author: Abhishek_Nayak1
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+print(dataset)
+
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in  set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+Y = dataset.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, Y_train)
+Y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_pred)
